dfe53a3beac69c784f3bb0d70f7725fa2672425d
jonibek99 <jonibekannaev5@gmail.com>
2025-05-16 17:06:11 +0500

    api

--- auto_reply.py
+++ auto_reply.py
@@ -1,37 +1,24 @@
 # import requests
 from config import TOKEN, BASE_URL
 
-
-# def reply_to_last_user():
-#     """
-#     Task 3: Auto-Reply System
-#     - Get most recent update
-#     - Extract chat_id
-#     - Send response message
-#     """
-#     TOKEN='7908519478:AAGWAJ14YjDrrAu5UeYKUmESP5xTONj5I'
-#     BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
-#     response=requests.get(f"{BASE_URL}/getUpdates")
-#     data=response.json()
-#     if not data.get('result'):
-#         print('no massage')
-#         return 
-#     last=data['result'][-1]
-#     chat_id=last['massege']['id']
-#     requests.post(f"{BASE_URL}/sendMessage",data={'chat_id':data['chat_id'],'text':'bu men egang'})
-#     print('javob yuoruldi')
 # reply_to_last_user()
 import requests
 
 def reply_to_last_user():
-
     response = requests.get(f"{BASE_URL}/getUpdates")
     data=response.json()
     updates=data['result'][-1]
-    massege=updates['message']['chat']['id']
-    sender=BASE_URL+"/sendMessage"
+    chat_id = updates['message']['chat']['id']
+    massege = updates['message']['text'].lower()
 
-    requests.post(sender,params={'chat_id':massege,'text':'ali kut pro max,'})
-    return massege
+    if 'hi' in massege:
+        reply_text = 'Hello!'
+    elif 'by' in massege:
+        reply_text = 'Goodbye!'
+    else:
+        reply_text ='xato'
+    send_url = BASE_URL + "/sendMessage"
+    requests.post(send_url, params={'chat_id': chat_id,'text':reply_text })
+    print(send_url)
+reply_to_last_user()
 
-print(reply_to_last_user())
