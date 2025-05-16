5194891e86452164fda7c5ab41ff68205a1d3796
jonibek99 <jonibekannaev5@gmail.com>
2025-05-16 16:51:33 +0500

    api4

--- message_handler.py
+++ message_handler.py
@@ -1,7 +1,22 @@
+import requests
+from config import TOKEN, BASE_URL
 def handle_message(message):
     """
     Optional Challenge: Conditional Responses
     - Handle different message inputs
     - Return appropriate responses
     """
-    pass
+    response = requests.get(f"{BASE_URL}/getUpdates")
+    data=response.json()
+    updates=data['result'][-1]
+    massege=updates['message']['text']
+    sendler=BASE_URL+"/sendMessage"
+
+    requests.post(sendler,params={'chat_id':massege, 'hello':'by'})
+    if 'hi' in sendler:
+        return 'hello'
+    elif 'by':
+     return 'gppdb'
+    print(massege)
+handle_message('me')
+  
